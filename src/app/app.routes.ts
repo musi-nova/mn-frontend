7c557707c6dd163b8a2341394e79cd84168e9c32
James Twose <james.twose2711@gmail.com>
2025-01-01 14:15:58 +0000

    adding a plans page, it's not connected to the API yet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { PlansComponent } from './components/plans/plans.component';
 
 export const routes: Routes = [
-    { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'home', component: HomeComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'plans', component: PlansComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
